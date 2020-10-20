ceee90bfea4d8e10c334d97e81f723db7eacd58f
Oliver Gutsche <oguatworld@gmail.com>
2020-10-20 13:13:24 -0500

    changes overdue for commit

--- run2stop1l/samples.py
+++ run2stop1l/samples.py
@@ -2,14 +2,12 @@ LUMI = 35900 # in pb
 
 ###
 SAMPLES_PREFIX = 'root://eospublic.cern.ch//eos/user/g/gutsche/data/'
-SAMPLES_BASE   = 'stopRun2/'
+SAMPLES_BASE   = 'run2data/'
 
 ####
 samples = {    
     'TTbar' : {
-        'filenames' : ['ttbar_singleLeptFromT_madgraph_pythia8_25ns.root', \
-#                        'ttbar_singleLeptFromT_madgraph_pythia8_25ns_1.root', \
-#                        'ttbar_singleLeptFromT_madgraph_pythia8_25ns_2.root' \
+        'filenames' : ['ttbar_powheg_pythia8_25ns.root', \
                       ],
         'xsec'      : None,   
         'eff'       : 1.,   
@@ -17,8 +15,8 @@ samples = {
         'weight'    : 1.,   
         'color'     : 'black',   
     },
-    'W3JetsToLNu' : {
-        'filenames' : ['W3JetsToLNu_madgraph_pythia8_25ns.root', \
+    'WZ' : {
+        'filenames' : ['WZ_pythia8_25ns.root', \
                       ],
         'xsec'      : None,   
         'eff'       : 1.,   
